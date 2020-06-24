7ff46a09b2d1b5ed6c8495656b26b8db37724bb6
aksel avetisyan <hello.dotoday@gmail.com>
2020-06-24 14:44:34 +0400

    homework 11

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ function getEmployers() {
                 addDataElement(v.employee_name, div)
                 addDataElement(v.employee_age, div)
                 addDataElement(`${v.employee_salary}֏`, div)
-                div.appendChild(addIcons('img/delete-icon.png', div, v.id))
                 employersListing.appendChild(div)
                 window.localStorage.setItem('employers', employersListing.innerHTML)
             })
@@ -28,24 +27,4 @@ function addDataElement(name, parentElement) {
     const div = document.createElement('div')
     div.textContent = name
     parentElement.appendChild(div)
-}
-
-function addIcons(src, parentElement, deleteId) {
-    const iconsList = document.createElement('div')
-    const deleteIcon = document.createElement('img')
-    deleteIcon.src = src
-    iconsList.classList.add('icons-list')
-    iconsList.appendChild(deleteIcon)
-    parentElement.appendChild(iconsList)
-    deleteIcon.onclick = function () {
-        deleteEmployer(deleteId).then(res => {
-            console.log(res)
-        })
-    }
-    return iconsList
-}
-
-async function deleteEmployer(id) {
-    let res = await fetch(`http://dummy.restapiexample.com/api/v1/delete/${id}`, {method: 'delete'})
-    if (res.status === 200) return res.json()
 }
